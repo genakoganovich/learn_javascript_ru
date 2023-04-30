18c9d96474ac4eeb23e391f953fbf14d763a2334
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-04-30 21:14:42 +0300

    0007: conditional branch start

--- 001_bacis/009_comparison_operators/009_001_script.js
+++ 001_bacis/009_comparison_operators/009_001_script.js
@@ -1,1 +1,7 @@
-// start
+console.log(5 > 4);
+console.log("ананас" > "яблоко");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
