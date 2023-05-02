b471d93e18e191c0639406bde08727a38ad6e1c8
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-05-02 22:40:45 +0300

    0010: locigal operators done

---
+++ 001_bacis/011_logical_operators/011_009_script.js
@@ -0,0 +1,16 @@
+let user = prompt('Кто там?');
+if (user === 'Админ') {
+    let password =  prompt('Пароль?');
+    if (password === 'Я Главный') {
+        alert('Здравствуйте!');
+    } else if (password === null || password === '') {
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+}
+else if (user === null || user === '') {
+    alert('Отменено');
+} else {
+    alert('Я вас не знаю');
+}
